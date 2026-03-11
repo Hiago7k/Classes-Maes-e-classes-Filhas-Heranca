282b9e6ed88bafb55f83b5f47185fdeb78d8c03e
Hiago 7k <hiago7k@gmail.com>
2026-03-11 20:19:02 -0300

    ajustando codigo

--- BancoDeDados.cs
+++ BancoDeDados.cs
@@ -1,14 +1,15 @@
 ﻿namespace Classes_Maes_e_classes_Filhas_Heranca;
 
- class BancoDeDados
+class BancoDeDados
 {
     List<Produto> produtos = new List<Produto>()
     {
-        new ProdutoDigital("Curso", "OO em C#", 100.00m, "Imagem ilustrativa", "Link")
+        new ProdutoDigital("Curso", "OO em C#", 100.00m, "Imagem ilustrativa", "Link"),
         new ProdutoFisico("Teclado", "Modelo compacto", 120.00m, "Imagem ilustrativa")
 
     };
-
+    
+    
 
     // Metodo com um retorno generico
     public Produto BuscarProdutoPeloNome(string nome)
